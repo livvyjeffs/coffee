603d928b603aa75f746b27d5fd280296d6ba2c1c
livvyjeffsgit <olivia@tribesay.com>
2015-05-08 03:48:38 +0800

    done for the night

--- app-maps/client/maps-client.js
+++ app-maps/client/maps-client.js
@@ -71,86 +71,43 @@ Template.map.helpers({
 Template.map.onCreated(function() {  
   GoogleMaps.ready('exampleMap', function(map) {
 
+    drawMap(map);
+    
     centerMap(map, Session.get('latitude_center'), Session.get('longitude_center'));
 
-    drawMap(map);
-
-//     google.maps.event.addListener(map.instance, 'click', function(event) {
-//       Markers.insert({ lat: event.latLng.lat(), lng: event.latLng.lng() });
-//     });
-
-//     var markers = {};
-
-//     Markers.find().observe({  
-//       added: function(document) {
-//     // Create a marker for this document
-//     var marker = new google.maps.Marker({
-//       draggable: true,
-//       animation: google.maps.Animation.DROP,
-//       position: new google.maps.LatLng(document.lat, document.lng),
-//       map: map.instance,
-//       // We store the document _id on the marker in order 
-//       // to update the document within the 'dragend' event below.
-//       id: document._id
-//     });
-
-//     // This listener lets us drag markers on the map and update their corresponding document.
-//     google.maps.event.addListener(marker, 'dragend', function(event) {
-//       Markers.update(marker.id, { $set: { lat: event.latLng.lat(), lng: event.latLng.lng() }});
-//     });
-
-//     // Store this marker instance within the markers object.
-//     markers[document._id] = marker;
-//   },
-//   changed: function(newDocument, oldDocument) {
-//     markers[newDocument._id].setPosition({ lat: newDocument.lat, lng: newDocument.lng });
-//   },
-//   removed: function(oldDocument) {
-
-//     // Remove the marker from the map
-//     markers[oldDocument._id].setMap(null);
-
-//     // Clear the event listener
-//     google.maps.event.clearInstanceListeners(
-//       markers[oldDocument._id]);
-
-//     // Remove the reference to this marker instance
-//     delete markers[oldDocument._id];
-//   }
-// });
-
-});
+  });
 });
 
 function centerMap(map, lat, lng){
   //hopefully the lat and lngs (which are hooked to session variables in implementation) will keep this reactive
   map.instance.setCenter(new google.maps.LatLng(lat,lng));
   console.log('NEW LAT LONG: ' + lat + ', ' + lng);
-}
 
-function drawMap(map){
+  var circleOptions = { 
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: '#FF0000',
+    fillOpacity: 0.35,
+    map: map.instance,
+    center: new google.maps.LatLng(Session.get('latitude_current'), Session.get('longitude_current')),
+    radius: Session.get('radius')
+  };
 
-  var shops = ShopList.find({}, {sort: {speed_down: -1}});
+    // Add the circle for this city to the map.
+    var radius = new google.maps.Circle(circleOptions);
 
-  map_draw_count++;
+  }
+
+  function drawMap(map){
+
+    map_draw_count++;
 
   // GoogleMaps.ready(map, function(map) {
 
     console.log(map_draw_count + ': map ready to be drawn');
 
-    var circleOptions = { 
-      strokeColor: '#FF0000',
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: '#FF0000',
-      fillOpacity: 0.35,
-      map: map.instance,
-      center: new google.maps.LatLng(Session.get('latitude_current'), Session.get('longitude_current')),
-      radius: Session.get('radius')
-    };
-
-    // Add the circle for this city to the map.
-    var radius = new google.maps.Circle(circleOptions);
+    var shops = ShopList.find({}, {sort: {speed_down: -1}});
 
     shops.forEach(function (theshop) {
 
