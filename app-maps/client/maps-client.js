424a00757b7ce202cd6bae3ece712031028ea528
livvyjeffsgit <olivia@tribesay.com>
2015-05-08 01:20:25 +0800

    added reactivity to drawMap function
    synced at 1 am before I commented

--- app-maps/client/maps-client.js
+++ app-maps/client/maps-client.js
@@ -2,18 +2,6 @@ console.log('**file: app-maps/client/maps-client.js loaded');
 
 Markers = new Mongo.Collection('markers'); 
 
-// var map_initialized = false;
-
-// var timer_counts = {
-//   map_intialize: 0,
-//   map_populate: 0,
-//   coordinate: 0,
-// };
-
-// var mapInitializeTimer = setInterval(initializeMap, 500);
-// var populateTimer = setInterval(populateMaps, 2000);
-// var displayCoordTimer = setInterval(checkDisplayCoords, 500);
-
 Meteor.startup(function() {
 
   // get current position
@@ -40,14 +28,13 @@ function getPosition(position) {
 }
 
 Template.map.helpers({
-  // This is a helper function, so it is a 'reactive composition'
+  // This is a helper function, so it is a 'reactive computation'
   // Any time a Session variable inside it changes, the function surrounding it will run again
   // https://www.discovermeteor.com/blog/reactivity-basics-meteors-magic-demystified/
   exampleMapOptions: function() {
 
     if(GoogleMaps.loaded()){
 
-      map_initialized = true;
 
       console.log('####### GOOGLE MAPS INITIALIZED at ' + Session.get('latitude_center') + ', ' + Session.get('longitude_center'));
 
@@ -66,119 +53,64 @@ Template.map.helpers({
 Template.map.onCreated(function() {  
   GoogleMaps.ready('exampleMap', function(map) {
 
-    drawMap(map);
+    drawMap(map, Session.get('latitude_center'), Session.get('longitude_center'));
 
-    google.maps.event.addListener(map.instance, 'click', function(event) {
-      Markers.insert({ lat: event.latLng.lat(), lng: event.latLng.lng() });
-    });
+//     google.maps.event.addListener(map.instance, 'click', function(event) {
+//       Markers.insert({ lat: event.latLng.lat(), lng: event.latLng.lng() });
+//     });
 
-    var markers = {};
+//     var markers = {};
 
-    Markers.find().observe({  
-      added: function(document) {
-    // Create a marker for this document
-    var marker = new google.maps.Marker({
-      draggable: true,
-      animation: google.maps.Animation.DROP,
-      position: new google.maps.LatLng(document.lat, document.lng),
-      map: map.instance,
-      // We store the document _id on the marker in order 
-      // to update the document within the 'dragend' event below.
-      id: document._id
-    });
+//     Markers.find().observe({  
+//       added: function(document) {
+//     // Create a marker for this document
+//     var marker = new google.maps.Marker({
+//       draggable: true,
+//       animation: google.maps.Animation.DROP,
+//       position: new google.maps.LatLng(document.lat, document.lng),
+//       map: map.instance,
+//       // We store the document _id on the marker in order 
+//       // to update the document within the 'dragend' event below.
+//       id: document._id
+//     });
 
-    // This listener lets us drag markers on the map and update their corresponding document.
-    google.maps.event.addListener(marker, 'dragend', function(event) {
-      Markers.update(marker.id, { $set: { lat: event.latLng.lat(), lng: event.latLng.lng() }});
-    });
+//     // This listener lets us drag markers on the map and update their corresponding document.
+//     google.maps.event.addListener(marker, 'dragend', function(event) {
+//       Markers.update(marker.id, { $set: { lat: event.latLng.lat(), lng: event.latLng.lng() }});
+//     });
 
-    // Store this marker instance within the markers object.
-    markers[document._id] = marker;
-  },
-  changed: function(newDocument, oldDocument) {
-    markers[newDocument._id].setPosition({ lat: newDocument.lat, lng: newDocument.lng });
-  },
-  removed: function(oldDocument) {
+//     // Store this marker instance within the markers object.
+//     markers[document._id] = marker;
+//   },
+//   changed: function(newDocument, oldDocument) {
+//     markers[newDocument._id].setPosition({ lat: newDocument.lat, lng: newDocument.lng });
+//   },
+//   removed: function(oldDocument) {
 
-    // Remove the marker from the map
-    markers[oldDocument._id].setMap(null);
+//     // Remove the marker from the map
+//     markers[oldDocument._id].setMap(null);
 
-    // Clear the event listener
-    google.maps.event.clearInstanceListeners(
-      markers[oldDocument._id]);
+//     // Clear the event listener
+//     google.maps.event.clearInstanceListeners(
+//       markers[oldDocument._id]);
 
-    // Remove the reference to this marker instance
-    delete markers[oldDocument._id];
-  }
-});
-
-});
-});
-
-// function populateMaps() {
-//   if(map_initialized){
-//     clearInterval(populateTimer);
-//     drawMap('exampleMap',Session.get('regional'),Session.get('region'));
-//   }else if(timer_counts.map_populate > 5){
-//     console.log('END: map population timed out');
-//     clearInterval(populateTimer);
-//   }else{
-//     timer_counts.map_populate++;
-//     console.log('//////// This map has not been initialized yet.');
+//     // Remove the reference to this marker instance
+//     delete markers[oldDocument._id];
 //   }
+// });
 
-// }
+});
+});
 
-// function checkDisplayCoords(){
-//   if(Session.get('regional')!==undefined){
-//     clearInterval(displayCoordTimer);
-//     setDisplayCoordinates(Session.get('regional'), Session.get('region'));
-//   }else if(timer_counts.coordinate>5){
-//     console.log('END: coordinates timed out');
-//     clearInterval(displayCoordTimer);
-//   }else{
-//     timer_counts.coordinate++;
-//     console.log('//////// Regional Coords not Set Try Again in 0.5s');
-//   }
-// }
-
-// function setDisplayCoordinates(regional, region){
-//   if(regional){
-
-//     Session.set('zoom', 11);
-//     Session.set('radius',10000);
-
-//     if(region==='beijing'){
-//      Session.set('latitude_center',39.903601);
-//      Session.set('longitude_center',116.387159);
-//    }else if(region==='bangkok'){
-//      Session.set('latitude_center',13.741943);
-//      Session.set('longitude_center',100.548653);
-//    }
-
-//  }else{
-//   // Session.set('zoom',15);
-//   // Session.set('radius',1000);
-
-//  //  if(Session.get('latitude_current') === undefined){
-//  //    //if unknown set to Saskatchewan, Canada
-//  //    Session.set('latitude_center',55.707526);
-//  //    Session.set('longitude_center',-105.476872);
-//  //    console.log('current location unknown');
-//  //  }else{
-//  //   Session.set('latitude_center',Session.get('latitude_current'));
-//  //   Session.set('longitude_center',Session.get('longitude_current'));
-//  // }
-
- 
-// }
-// }
-
-function drawMap(map){
+function drawMap(map, lat, lng){
 
   // GoogleMaps.ready(map, function(map) {
 
     console.log('map ready to be drawn');
+
+    //hopefully the lat and lngs (which are hooked to session variables in implementation) will keep this reactive
+
+    map.instance.setCenter(new google.maps.LatLng(lat,lng));
 
     var circleOptions = { 
       strokeColor: '#FF0000',
