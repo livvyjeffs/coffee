5d88e015bc7133b04df6454bddb3a40dc671797d
livvyjeffsgit <olivia@tribesay.com>
2015-05-21 11:57:14 +0700

    stripe package added limited support

--- client/views/subscribe/subscribe.js
+++ client/views/subscribe/subscribe.js
@@ -15,20 +15,32 @@ Template.subscribe.events({
 
 		$(template).find('status').text("You've purchased the dataset, thank you!");
 
-		ccNum = $('#ccnum').val();
-		cvc = $('#cvc').val();
-		expMo = $('#exp-month').val();
-		expYr = $('#exp-year').val();
+		// ccNum = $('#ccnum').val();
+		// cvc = $('#cvc').val();
+		// expMo = $('#exp-month').val();
+		// expYr = $('#exp-year').val();
 
-		Stripe.card.createToken({
-			number: ccNum,
-			cvc: cvc,
-			exp_month: expMo,
-			exp_year: expYr,
-		}, function(status, response) {
-			stripeToken = response.id;
-			Meteor.call('chargeCard', stripeToken);
-		});
+		// Stripe.card.createToken({
+		// 	number: ccNum,
+		// 	cvc: cvc,
+		// 	exp_month: expMo,
+		// 	exp_year: expYr,
+		// }, function(status, response) {
+		// 	stripeToken = response.id;
+		// 	Meteor.call('chargeCard', stripeToken);
+		// });
+
+		// Stripe.charges.create({
+		// 	amount: 1000,
+		// 	currency: "USD",
+		// 	card: {
+		// 		number: "4242424242424242",
+		// 		exp_month: "03",
+		// 		exp_year: "2014"
+		// 	}
+		// }, function (err, res) {
+		// 	console.log(err, res);
+		// });
 
 		Meteor.call('addSubscription', Meteor.userId(), ['subscribed'], function() {
 			console.log(Meteor.user().username + ' is now subscribed');
