470acd0ed805c655985fc7cc4a5f32f5301eb368
livvyjeffsgit <olivia@tribesay.com>
2015-05-21 13:22:10 +0700

    YAYYYYYYYYYYYYYYYYYYYYY

--- client/views/subscribe/subscribe.js
+++ client/views/subscribe/subscribe.js
@@ -12,42 +12,9 @@ Template.subscribe.helpers({
 
 Template.subscribe.events({
 	"click button.purchase": function(event, template){
-
-		$(template).find('status').text("You've purchased the dataset, thank you!");
-
-		// ccNum = $('#ccnum').val();
-		// cvc = $('#cvc').val();
-		// expMo = $('#exp-month').val();
-		// expYr = $('#exp-year').val();
-
-		// Stripe.card.createToken({
-		// 	number: ccNum,
-		// 	cvc: cvc,
-		// 	exp_month: expMo,
-		// 	exp_year: expYr,
-		// }, function(status, response) {
-		// 	stripeToken = response.id;
-		// 	Meteor.call('chargeCard', stripeToken);
-		// });
-
-		// Stripe.charges.create({
-		// 	amount: 1000,
-		// 	currency: "USD",
-		// 	card: {
-		// 		number: "4424242424242422",
-		// 		exp_month: "03",
-		// 		exp_year: "2014"
-		// 	}
-		// }, function (err, res) {
-		// 	console.log(err, res);
-		// });
-
-		// Meteor.call('addSubscription', Meteor.userId(), ['subscribed'], function() {
-		// 	console.log(Meteor.user().username + ' is now subscribed');
-		// });
-
-},
-'submit form': function (e) {
+		
+	},
+	'submit form': function (e, template) {
 		e.preventDefault();
 	  // make sure there is a Meteor.user() or wrap the following inside the Meteor.createUser callback function
 	  var easy = StripeEasy.submitHelper(e);
@@ -59,6 +26,11 @@ Template.subscribe.events({
 	  }else{
 	  	console.log('paymet success!')
 	  	console.log(result);
+
+	  	$(template).find('status').text("You've purchased the dataset, thank you!");
+	  	Meteor.call('addSubscription', Meteor.userId(), ['subscribed'], function() {
+	  		console.log(Meteor.user().username + ' is now subscribed');
+	  	});
 	  }
 	    // if no error, will return the newly created customer object from Stripe
 	});
